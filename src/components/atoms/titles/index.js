fe86d28c1ad45d17c46b8cf58260e05292a623ea
Cat Miller <catmiller.cm@gmail.com>
2025-04-25 19:03:49 -0400

    adjust titles

--- src/components/atoms/titles/index.js
+++ src/components/atoms/titles/index.js
@@ -3,8 +3,8 @@ import styles from './titles.module.css';
 const Titles = () => {
   return (
     <div className={styles.titles}>
-      <h1>Create my Samba mix!</h1>
-      <h3>Start by trying out the sounds and then start recording when you're ready!</h3>
+      <h1>SambaStack</h1>
+      <h4>Start by trying out the sounds and then start recording when you're ready!</h4>
     </div>
   )
 };
